619c875e7dc3e0972c7d63bef08575a00fec2300
spoorn <spookump@gmail.com>
2022-09-25 04:26:01 -0700

    Change static variables to const if applicable

--- src/util/constants.rs
+++ src/util/constants.rs
@@ -1,16 +1,16 @@
 use once_cell::sync::Lazy;
 use rusttype::{Font, Scale};
 
-pub static RGB_TO_GREYSCALE: (f32, f32, f32) = (0.299, 0.587, 0.114);
+pub const RGB_TO_GREYSCALE: (f32, f32, f32) = (0.299, 0.587, 0.114);
 // Font height of ascii when producing videos, approximately the number of pixels
-pub static FONT_HEIGHT: f32 = 12.0;
-pub static FONT_SCALE: Scale = Scale {
+pub const FONT_HEIGHT: f32 = 12.0;
+pub const FONT_SCALE: Scale = Scale {
     x: FONT_HEIGHT,
     y: FONT_HEIGHT,
 };
 // When creating the output ascii video, for Cascadia font, this is a magic height to width ratio
 // for the video dimensions so the text fits to the frames' ends
-pub static MAGIC_HEIGHT_TO_WIDTH_RATIO: f32 = 2.048;
+pub const MAGIC_HEIGHT_TO_WIDTH_RATIO: f32 = 2.048;
 pub static CASCADIA_FONT: Lazy<Font<'static>> = Lazy::new(|| {
     let font_data = include_bytes!("fonts/Cascadia.ttf");
     Font::try_from_bytes(font_data).unwrap()
