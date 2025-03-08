54605606cbc81c86214d88d210e6be4690061af1
Preston O'Neal <prestononeal@gmail.com>
2025-03-07 22:20:10 -0600

    share code

--- src/image/image.rs
+++ src/image/image.rs
@@ -97,50 +97,8 @@ pub fn write_to_image<S: AsRef<str>>(
 }
 
 #[inline]
-pub fn convert_image_to_ascii(config: &ImageConfig) -> Vec<Vec<String>> {
-    let img_path = config.image_path.as_str();
-    check_valid_file(img_path);
-    let scale_down = config.scale_down;
-    let height_sample_scale = config.height_sample_scale;
-
-    // Use custom chars if provided, otherwise use default ramp
-    let greyscale_ramp: Vec<String> = if let Some(ref chars) = config.custom_chars {
-        chars.clone()
-    } else {
-        if config.invert {
-            REVERSE_GREYSCALE_RAMP.iter().map(|s| s.to_string()).collect()
-        } else {
-            GREYSCALE_RAMP.iter().map(|s| s.to_string()).collect()
-        }
-    };
-
-    let img = image::open(img_path).unwrap_or_else(|_| panic!("Image at {img_path} could not be opened"));
-    let (width, height) = img.dimensions();
-    let scaled_width = (width as f32 / scale_down) as usize;
-    let scaled_height = ((height as f32 / scale_down) / height_sample_scale) as usize;
-
-    let mut res = vec![vec![" ".to_string(); scaled_width]; scaled_height];
-
-    for (y, row) in res.iter_mut().enumerate() {
-        for x in 0..scaled_width {
-            let pix =
-                img.get_pixel((x as f32 * scale_down) as u32, (y as f32 * scale_down * height_sample_scale) as u32);
-            if pix[3] != 0 {
-                let greyscale_value = RGB_TO_GREYSCALE.0 * pix[0] as f32
-                    + RGB_TO_GREYSCALE.1 * pix[1] as f32
-                    + RGB_TO_GREYSCALE.2 * pix[2] as f32;
-                let index = (greyscale_value * (greyscale_ramp.len() - 1) as f32 / 255.0).ceil() as usize;
-                row[x] = greyscale_ramp[index].clone();
-            }
-        }
-    }
-
-    res
-}
-
-#[inline]
-pub fn convert_image_bytes_to_ascii(
-    image_bytes: &[u8],
+fn convert_image_to_ascii_internal(
+    img: image::DynamicImage,
     config: &ImageConfig,
 ) -> Vec<Vec<String>> {
     let scale_down = config.scale_down;
@@ -157,8 +115,6 @@ pub fn convert_image_bytes_to_ascii(
         }
     };
 
-    let img = image::load_from_memory(image_bytes)
-        .unwrap_or_else(|_| panic!("Could not load image from memory"));
     let (width, height) = img.dimensions();
     let scaled_width = (width as f32 / scale_down) as usize;
     let scaled_height = ((height as f32 / scale_down) / height_sample_scale) as usize;
@@ -169,7 +125,8 @@ pub fn convert_image_bytes_to_ascii(
         for x in 0..scaled_width {
             let pix =
                 img.get_pixel((x as f32 * scale_down) as u32, (y as f32 * scale_down * height_sample_scale) as u32);
-            if pix[3] != 0 {
+            if pix[3] > 127 {  // Apply a transparency threshold
+                // Standard luminance formula
                 let greyscale_value = RGB_TO_GREYSCALE.0 * pix[0] as f32
                     + RGB_TO_GREYSCALE.1 * pix[1] as f32
                     + RGB_TO_GREYSCALE.2 * pix[2] as f32;
@@ -180,6 +137,24 @@ pub fn convert_image_bytes_to_ascii(
     }
 
     res
+}
+
+#[inline]
+pub fn convert_image_to_ascii(config: &ImageConfig) -> Vec<Vec<String>> {
+    let img_path = config.image_path.as_str();
+    check_valid_file(img_path);
+    let img = image::open(img_path).unwrap_or_else(|_| panic!("Image at {img_path} could not be opened"));
+    convert_image_to_ascii_internal(img, config)
+}
+
+#[inline]
+pub fn convert_image_bytes_to_ascii(
+    image_bytes: &[u8],
+    config: &ImageConfig,
+) -> Vec<Vec<String>> {
+    let img = image::load_from_memory(image_bytes)
+        .unwrap_or_else(|_| panic!("Could not load image from memory"));
+    convert_image_to_ascii_internal(img, config)
 }
 
 pub fn process_image(config: ImageConfig) {
